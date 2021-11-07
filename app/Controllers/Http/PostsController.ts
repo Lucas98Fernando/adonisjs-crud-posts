b497a2c4b097247e848c2f0ca03ce0512fc643e9
Lucas Fernando <lucas98fernando@gmail.com>
2021-11-06 22:37:47 -0300

    Arquivos iniciais do CRUD de posts

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,39 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
+
+export default class PostsController {
+  // Listagem
+  public async index({}: HttpContextContract) {
+    const posts = await Post.all()
+    return posts
+  }
+
+  // Criação
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'description'])
+    const newPost = await Post.create(data)
+    return newPost
+  }
+
+  // Exibição específica
+  public async show({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    return post
+  }
+
+  // Atualização
+  public async update({ params, request }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const newData = request.only(['title', 'description'])
+    post.merge(newData)
+    await post.save()
+    return post
+  }
+
+  // Exclusão
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    await post.delete()
+    return post
+  }
+}
