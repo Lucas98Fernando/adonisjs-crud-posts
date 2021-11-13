8ef74945fa9c56fafad97422a3354b6f255f3ca3
Lucas Fernando <lucas98fernando@gmail.com>
2021-11-13 12:03:04 -0300

    Adicionando expiração de token e nova coluna na tabela de usuários

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,20 +2,28 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async register({ request }: HttpContextContract) {
-    const data = request.only(['email', 'password'])
-    const user = await User.create(data)
-    return user
+  public async register({ request, response }: HttpContextContract) {
+    try {
+      const data = request.only(['name', 'email', 'password'])
+      const user = await User.create(data)
+      return user
+    } catch {
+      response.badRequest({
+        message: 'Não foi possível realizar o cadastro',
+      })
+    }
   }
 
   public async login({ auth, request, response }: HttpContextContract) {
     const email = request.input('email')
     const password = request.input('password')
     try {
-      const token = await auth.use('api').attempt(email, password)
+      const token = await auth.use('api').attempt(email, password, {
+        expiresIn: '30mins',
+      })
       return token
     } catch {
-      return response.badRequest('Credenciais inválidas!')
+      return response.badRequest({ message: 'Credenciais inválidas!' })
     }
   }
 }
