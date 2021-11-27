87ae019eec43ca30fc50e3e63792ab084e0a1394
Lucas Fernando <lucas98fernando@gmail.com>
2021-11-27 16:38:50 -0300

    Ajuste na resposta de login

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -27,7 +27,7 @@ export default class UsersController {
       })
       return { token: token.token, name: auth.user?.name, email: auth.user?.email }
     } catch {
-      return response.badRequest({ message: 'Credenciais inválidas!' })
+      return response.badRequest({ message: 'Não foi possível fazer login' })
     }
   }
 
